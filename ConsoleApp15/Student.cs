10e554d15364a86daf17080a328c54b5d9e7a9a9
Mresu <161798697+Mresu@users.noreply.github.com>
2024-04-22 07:00:38 +0400

    Add files via upload

---
+++ ConsoleApp15/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Student
+    {
+        
+              private int Id()
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            public Student(int id, string name, string surname)
+            {
+            Id = id;
+                Name = name;
+                Surname = surname;
+            }
+        }
+    }
+
